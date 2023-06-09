ed5f56a431671e7f130962704fa2b03e62cfa3fb
default-007 <brianokola@gmail.com>
2023-06-09 21:41:22 +0300

    Create Healthcare provider

--- users/models.py
+++ users/models.py
@@ -9,4 +9,29 @@ class User(AbstractUser):
 
 
 class Patient(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True, related_name="patient"
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    other_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.IntegerField()
+    created_at = models.DateTimeField(default=timezone.now)
+    date_of_birth = models.DateField(blank=True)
+    weight = models.IntegerField()
+
+
+class Healthcare_Providers(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name="healthcare provider",
+    )
+    full_name = models.CharField(max_length=100)
+    contact = models.IntegerField()
+    email = models.EmailField()
+    physical_address = models.CharField(max_length=100)
+    registration_no = models.CharField(max_length=50)
+    created_at = models.DateTimeField(default=timezone.now)
