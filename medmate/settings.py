e2a7f19c6a61d5722950531e4dd29b10c4f50a76
default-007 <brianokola@gmail.com>
2023-06-10 19:45:12 +0300

    MInor changes to Providder model

--- medmate/settings.py
+++ medmate/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "users.CustomUser"
+AUTH_USER_MODEL = "users.User"
 LOGIN_REDIRECT_URL = "student-home"
 LOGIN_URL = "login"
--- users/models.py
+++ users/models.py
@@ -5,52 +5,47 @@ from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
 
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     username = None
     is_patient = models.BooleanField(default=False)
     is_provider = models.BooleanField(default=False)
-
-
-class Patient(models.Model):
-    user = models.OneToOneField(
-        CustomUser, on_delete=models.CASCADE, primary_key=True, related_name="patient"
-    )
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    other_name = models.CharField(max_length=100)
     email = models.EmailField(_("email address"), unique=True)
-
-    contact = models.IntegerField()
-    created_at = models.DateTimeField(default=timezone.now)
-    date_of_birth = models.DateField(blank=True)
-    weight = models.IntegerField()
+    physical_address = models.CharField(max_length=100)
     USERNAME_FIELD = "email"
-    REQUIRED_FIELD = []
+
     objects = CustomUserManager()
 
     def __str__(self):
         return self.email
 
 
+class Patient(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True, related_name="patient"
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    other_name = models.CharField(max_length=100)
+    contact = models.IntegerField()
+    created_at = models.DateTimeField(default=timezone.now)
+    date_of_birth = models.DateField(blank=True)
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+
 class HealthcareProviders(models.Model):
     user = models.OneToOneField(
-        CustomUser,
+        User,
         on_delete=models.CASCADE,
         primary_key=True,
-        related_name="healthcare provider",
+        related_name="healthcare_provider",
     )
     full_name = models.CharField(max_length=100)
-    contact = models.IntegerField()
-    email = models.EmailField()
     is_active = models.BooleanField(default=False)
-    physical_address = models.CharField(max_length=100)
     registration_no = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(default=timezone.now)
-
-    USERNAME_FIELD = "registration_no"
-    REQUIRED_FIELD = []
-
-    objects = CustomUserManager
 
     def __str__(self):
         return self.full_name
