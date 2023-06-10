abe6ebde799839d0c8ac510c46be8cd4a84d14a6
default-007 <brianokola@gmail.com>
2023-06-10 19:03:01 +0300

    MInor changes to Providder model

--- medmate/settings.py
+++ medmate/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "users.CustomUser"
 LOGIN_REDIRECT_URL = "student-home"
 LOGIN_URL = "login"
--- users/models.py
+++ users/models.py
@@ -1,30 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
 
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
+    username = None
     is_patient = models.BooleanField(default=False)
     is_provider = models.BooleanField(default=False)
 
 
 class Patient(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, primary_key=True, related_name="patient"
+        CustomUser, on_delete=models.CASCADE, primary_key=True, related_name="patient"
     )
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     other_name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(_("email address"), unique=True)
+
     contact = models.IntegerField()
     created_at = models.DateTimeField(default=timezone.now)
     date_of_birth = models.DateField(blank=True)
     weight = models.IntegerField()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELD = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
 
 
-class Healthcare_Providers(models.Model):
+class HealthcareProviders(models.Model):
     user = models.OneToOneField(
-        User,
+        CustomUser,
         on_delete=models.CASCADE,
         primary_key=True,
         related_name="healthcare provider",
@@ -32,6 +42,15 @@ class Healthcare_Providers(models.Model):
     full_name = models.CharField(max_length=100)
     contact = models.IntegerField()
     email = models.EmailField()
+    is_active = models.BooleanField(default=False)
     physical_address = models.CharField(max_length=100)
-    registration_no = models.CharField(max_length=50)
+    registration_no = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "registration_no"
+    REQUIRED_FIELD = []
+
+    objects = CustomUserManager
+
+    def __str__(self):
+        return self.full_name
