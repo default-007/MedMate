6b1e9183e9edb2d4c1df4edd6372b15bdbac5137
default-007 <brianokola@gmail.com>
2023-06-19 20:42:31 +0300

    Evrnts modeulr

--- mms/models.py
+++ mms/models.py
@@ -8,5 +8,32 @@ class Institution(models.Model):
     )
 
 
-class Schedule(models.Model):
-    day = models.CharField(max_length=50)
+class EventManager(models.Manager):
+    """Event Manager"""
+    def get_all_events(self, user):
+        events = Event.objects.filter(user=user, is_active=True, is_deleted=False)
+        return events
+
+    def get_running_events(self, user):
+        running_events = Event.objects.filter(user=user, is_active=True, is_deleted=False, end_time__gte=datetime.now().date(),).order_by("start_time")
+        return running_events
+
+class Event(EventAbstract):
+    """Event models"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="events")
+    title = models.CharField(max_length=200, unique=True)
+    description = models.DateTimeField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    objects = EventManager()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("mms:event-detail", args=(self.id,))
+
+    @property
+    
+    
